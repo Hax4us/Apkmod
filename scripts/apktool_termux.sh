0c0d1513b51d9dffebbb32599a3237fbc50a7d63
Hax4us <lkpandey950@gmail.com>
2019-09-04 08:31:40 +0530

    fix path of alpine

--- scripts/apktool_termux.sh
+++ scripts/apktool_termux.sh
@@ -2,4 +2,4 @@
 unset LD_PRELOAD
 args="$@"
 comnd="apktool $args"
-exec proot --link2symlink -0 -r ${HOME}/TermuxAlpine/ -b /dev/ -b /sys/ -b /proc/ -b /sdcard -b $HOME -w $HOME /usr/bin/env HOME=/root TERM="$TERM" LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/sh --login -c "$comnd"
+exec proot --link2symlink -0 -r ${PREFIX}/share/TermuxAlpine/ -b /dev/ -b /sys/ -b /proc/ -b /sdcard -b $HOME -w $HOME /usr/bin/env HOME=/root TERM="$TERM" LANG=$LANG PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/sh --login -c "$comnd"
