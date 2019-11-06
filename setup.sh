d3233e66cda486bf5d70ffe527b639a1d0f6752d
Hax4us <lkpandey950@gmail.com>
2019-11-06 12:01:03 +0530

    remove extra overhead of maintaining patches

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ setup_alpine() {
 		fi
 	fi
 	if [ "${noinstall}" = "no" ]; then
-		wget https://raw.githubusercontent.com/Hax4us/TermuxAlpine/master/TermuxAlpine.sh
+		wget https://raw.githubusercontent.com/Hax4us/TermuxAlpine/master/TermuxAlpine.sh -O TermuxAlpine.sh
 		bash TermuxAlpine.sh
 	fi
 	mkdir ${ALPINEDIR}/root/.bind
@@ -58,7 +58,7 @@ install_deps() {
 			;;
 	esac
 	aapturl=https://github.com/hax4us/Apkmod/raw/master/aapt/${ARCH}/aapt.tar.gz
-	wget ${aapturl} && tar -xf aapt.tar.gz -C ${LIBDIR} && rm aapt.tar.gz
+	wget ${aapturl} -O aapt.tar.gz && tar -xf aapt.tar.gz -C ${LIBDIR} && rm aapt.tar.gz
     for i in aapt aapt2; do
         mv ${LIBDIR}/android/${i} ${ALPINEDIR}/usr/bin
     done
@@ -72,16 +72,19 @@ install_deps() {
 
 install_scripts() {
 	for script in apktool_termux.sh apktool_alpine.sh apk.rb; do
-		wget https://github.com/hax4us/Apkmod/raw/master/scripts/${script}
+		wget https://github.com/hax4us/Apkmod/raw/master/scripts/${script} -O ${script}
 	done
 	mv apktool_termux.sh ${BINDIR}/apktool && chmod +x ${BINDIR}/apktool
 	mv apktool_alpine.sh ${ALPINEDIR}/bin/apktool && chmod +x ${ALPINEDIR}/bin/apktool
-	if [ -d ${HOME}/metasploit-framework ]; then
-        metasploit="noinbuilt"
-		mv apk.rb ${HOME}/metasploit-framework/lib/msf/core/payload/
+    if [ -d ${HOME}/metasploit-framework -a -d ${PREFIX}/opt/metasploit-framework ]; then
+        printf "${red}[!] More than one metasploit detected , remove anyone from them to install Apkmod\n${reset}"
+        exit 1
+    elif [ -d ${HOME}/metasploit-framework ]; then
+        msf_dir=${HOME}/metasploit-framework
+		mv apk.rb ${msf_dir}/lib/msf/core/payload/
 	elif [ -d ${PREFIX}/opt/metasploit-framework ]; then
-        metasploit="inbuilt"
-		mv apk.rb ${PREFIX}/opt/metasploit-framework/lib/msf/core/payload/
+        msf_dir=${PREFIX}/opt/metasploit-framework
+		mv apk.rb ${msf_dir}/lib/msf/core/payload/
 	else
 		printf "${red}[!] Metasploit is not installed${reset}"
         exit 1
@@ -89,28 +92,41 @@ install_scripts() {
 }
 
 do_patches() {
-    if [ "${metasploit}" = "noinbuilt" ]; then
-        VERSION=$(grep VERSION ~/metasploit-framework/lib/metasploit/framework/version.rb | head -n1 | sed -e 's/ /\n/g' | grep -E "[0-9]" | sed -e 's/"//g')
-        cd ~/metasploit-framework
-    elif [ "${metasploit}" = "inbuilt" ]; then
-        VERSION=$(grep VERSION ${PREFIX}/opt/metasploit-framework/lib/metasploit/framework/version.rb | head -n1 | sed -e 's/ /\n/g' | grep -E "[0-9]" | sed -e 's/"//g')
-        cd ${PREFIX}/opt/metasploit-framework
-    fi
-    if [ -z "$VERSION" ]; then
-        printf "${red}[!] metasploit version can't be determined${reset}"
-        exit 1
-    fi
-    for patch in msfvenom.patch payload_generator.rb.patch; do
-        rm -f ${patch}
-        wget https://github.com/hax4us/Apkmod/raw/master/patches/msf-${VERSION}/${patch}
+    cd ${msf_dir}
+    for i in "msfvenom" "lib/msf/core/payload_generator.rb"; do
+        if [ ! -e "${i}.orig" ]; then
+            cp ${i} ${i}.orig
+        fi
     done
-    patch -N --dry-run -i msfvenom.patch > /dev/null
-    if [ $? -eq 0 ]; then
-        patch -i msfvenom.patch
+    busybox grep "#patched" msfvenom
+    if [ $? -ne 0 ]; then
+        line_num=$(busybox grep -n "help" msfvenom | cut -d ":" -f1)
+        line_num=$((${line_num}-1))
+        busybox awk -v "n=${line_num}" -v "s=\n\topt.on('--use-aapt2','Use aapt2 for recompiling') do\n\t\topts[:use_aapt2] = true\n\tend" '(NR==n) { print s } 1' msfvenom.orig > msfvenom
+        if [ $? -eq 0 ]; then
+            printf "#patched" >> msfvenom
+        else
+            printf "${red}[!] can't patch msfvenom\n${reset}"
+            exit 1
+        fi
     fi
-    patch -p0 -N --dry-run -i payload_generator.rb.patch > /dev/null
-    if [ $? -eq 0 ]; then
-        patch -p0 -i payload_generator.rb.patch
+    busybox grep "#patched" lib/msf/core/payload_generator.rb
+    if [ $? -ne 0 ]; then
+        line_num=$(busybox grep -n ":add_code" lib/msf/core/payload_generator.rb | head -n1 | cut -d ":" -f1)
+        line_num=$((${line_num}-2))
+        busybox awk -v "n=${line_num}" -v "s=\t# @\!attribute  use_aapt2\n\t#   @return [String] use aapt2 or not\n\tattr_accessor :use_aapt2" '(NR==n) { print s } 1' lib/msf/core/payload_generator.rb.orig > lib/msf/core/payload_generator.rb
+        if [ $? -ne 0 ]; then
+            printf "${red}[!] can't patch payload_generator.rb\n${reset}"
+            exit 1
+        fi
+        line_num=$(busybox grep -n "apk_backdoor.backdoor_apk" lib/msf/core/payload_generator.rb | cut -d ":" -f1)
+        busybox sed -i "${line_num}s/.*/\t\traw_payload = apk_backdoor.backdoor_apk(template, generate_raw_payload, use_aapt2)/" lib/msf/core/payload_generator.rb
+        if [ $? -eq 0 ]; then
+            printf "#patched" >> lib/msf/core/payload_generator.rb
+        else
+            printf "${red}[!] can't patch payload_generator.rb\n${reset}"
+            exit 1
+        fi
     fi
 }
 
