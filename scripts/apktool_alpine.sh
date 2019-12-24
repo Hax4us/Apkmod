862498b4a34d4b1f767553ed01a35a7a0596b03e
Hax4Us <lkpandey950@gmail.com>
2019-12-24 12:29:19 +0530

    fix java exec issue

--- scripts/apktool_alpine.sh
+++ scripts/apktool_alpine.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+JAVA="/usr/lib/jvm/java-1.8-openjdk/bin/java"
 javaOpts="-Xmx512M -Dfile.encoding=utf-8"
-exec java $javaOpts -jar /opt/apktool.jar "$@"
+exec $JAVA $javaOpts -jar /opt/apktool.jar "$@"
