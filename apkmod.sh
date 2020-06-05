f51087892aa4a320c4902f159cf81c47cecd5506
Hax4Us <lkpandey950@gmail.com>
2020-06-05 15:07:35 +0530

    unset _JAVA_OPTIONS if set

--- apkmod.sh
+++ apkmod.sh
@@ -6,16 +6,17 @@
 # Email   : lkpandey950@gmail.com      #
 ########################################
 
+unset _JAVA_OPTIONS
 CWD=$(pwd)
 VERSION="2.0"
 AAPT=""
 
 #colors
-cyan='\033[1;36m'                       
-green='\033[1;32m'                      
-red='\033[1;31m'                        
-yellow='\033[1;33m'                     
-blue='\033[1;34m'                       
+cyan='\033[1;36m'                      
+green='\033[1;32m'                     
+red='\033[1;31m'
+yellow='\033[1;33m' 
+blue='\033[1;34m'                      
 purple='\033[1;35m'
 reset='\033[0m'
 
