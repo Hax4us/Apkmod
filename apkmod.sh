ca39f8922606edc9acac8aac53a88a9aa51d426a
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-12-12 08:32:21 +0530

    fixed updater

--- apkmod.sh
+++ apkmod.sh
@@ -8,6 +8,7 @@
 
 unset _JAVA_OPTIONS
 CWD=$(pwd)
+LOC=$(which apkmod)
 VERSION="2.2"
 AAPT=""
 
@@ -193,19 +194,34 @@ validate_input() {
 
 update() {
 	temp=$(curl -L -s https://github.com/Hax4us/Apkmod/raw/master/apkmod.sh | grep -w "VERSION=" | head -n1)
-	N_VERSION=$(echo ${temp} | sed -e 's/[^0-9]\+[^0-9]/ /g' | cut -d '"' -f1)
+	C_VERSION="$temp"
+        N_VERSION=$(grep '^VERSION=' $LOC)
+check_for_update() {
+        if [[ ${C_VERSION} == ${N_VERSION} ]];then
+        update_log="Apkmod already updated."
+        else
+        update_log="Apkmod update available."
+        fi
+}
 	if [ "${1}" != "-u" ]; then
-		[ 1 -eq $(echo "${N_VERSION} != ${VERSION}" | bc -l) ] && print_status "Update is available, run [ apkmod -u ] for update" && exit 1
+		check_for_update
+		print_status "$update_log"
+		[ 1 -eq $(echo "${C_VERSION} != ${N_VERSION}" | bc -l) ] && print_status "Update is available, run [ apkmod -u ] for update" && exit 1
 	fi
 	if [ "${1}" = "-u" ]; then
+		check_for_update
+		print_status "$update_log"
+		if [[ ${C_VERSION} != ${N_VERSION} ]];then
 		rm -f setup.sh
 		if [ "$2" = "--with-alpine" ]; then
+		print_status "$update_log"
 			ARGS=$2
 		else
 			ARGS=--without-alpine
 		fi
-		wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh && sh setup.sh $ARGS
+wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh && sh setup.sh $ARGS
 	fi
+    fi
 }
 
 ##############
@@ -259,9 +275,7 @@ while getopts ":z:d:r:s:b:o:hvuVR:-:" opt; do
             exit 0
             ;;
         u)
-            print_status "Updating ..."
-            update "-${opt}" "$2"
-            print_status "Update completed"
+	    update "-${opt}" "$2"
             exit 0
             ;;
         V)
